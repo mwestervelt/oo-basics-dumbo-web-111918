248c67f2008e30a8c34c68c1367bc6453b64b1c5
mwestervelt <elle.westervelt@gmail.com>
2018-11-22 12:58:56 -0500

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,13 @@
+class Book
 
+attr_accessor :title, :author, :page_count, :genre
 
+  def initialize(title)
+    @title = "And Then There Were None"
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,15 @@
-# Make your shoe class here!
+
+class Shoe
+
+attr_accessor :brand, :color, :size, :material, :condition
+
+  def initialize(brand)
+    @brand = "Nike"
+  end
+
+  def cobble
+    @condition= "new"
+    puts "Your shoe is as good as new!"
+  end
+
+end
